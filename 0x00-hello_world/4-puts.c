b86744283792f77506e894fc1a820c875f98a23f
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-08 17:25:59 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * mai - print a given phrase
- * Return: 0 if exited properly, otherwise non-zero
-*/
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
 
 int main (void)
 {  
