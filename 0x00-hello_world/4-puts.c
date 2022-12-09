33f50b9be21b4e02b32a2cb394fb2a34955b25ba
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-08 17:14:27 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * mai - print a given phrase
  * Return: 0 if exited properly, otherwise non-zero
- */
+*/
+
 int main (void)
 {  
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
