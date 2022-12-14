27f09d4443e41e2340f3efcfcc45fc7130c3b72a
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-13 17:43:38 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -18,6 +18,6 @@ int print_last_digit(int n)
 	}
 		else
 			last_digit = (n % 10);
-			_putchar (last_digit + '0');
-			return (last_digit);
+		_putchar(last_digit + '0');
+		return (last_digit);
 }
