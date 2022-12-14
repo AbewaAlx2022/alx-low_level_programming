7bfbf3b6cce41dff76544e7a7e095ff4d7b8d13e
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-13 18:43:35 -0800

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - ckeck the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putcher('\n');
+	print_times_table(5);
+	_piutchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
