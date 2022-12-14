1f8cb826af2e1d87cb88ab220527118275cd783f
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-13 17:53:36 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * times_table -> prints the 9 times table
+ * times_table - prints tables
+ *
+ * Return: Aleays 0.
  */
 
 void times_table(void)
@@ -28,18 +30,18 @@ void times_table(void)
 					_putchar(',');
 					_putchar(' ');
 				}
-				else
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
 				{
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-					if (b < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
