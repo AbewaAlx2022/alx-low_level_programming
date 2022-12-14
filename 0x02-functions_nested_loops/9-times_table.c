74e5984f0d3327c1aeb4b69f18d2690e4fc0c381
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-13 16:59:07 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * time_table - print the 9 times table
- * Return: 0 (Always Success)
+ * times_table -> prints the 9 times table
  */
+
 void times_table(void)
 {
 	int a, b, c;
