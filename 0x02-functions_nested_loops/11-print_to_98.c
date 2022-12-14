646b36e513049010b9147aaa0a09df64899d20f0
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-13 18:28:44 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,12 +6,11 @@
  * @n: the number to start counting from n to 98
  * Return: Always 0.
  */
-
 void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%i. ", n);
+		printf("%i, ", n);
 		n++;
 	}
 	while (n > 98)
