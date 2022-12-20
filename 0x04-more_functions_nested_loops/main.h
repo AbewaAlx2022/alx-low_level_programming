ea72080b28c96b44646ca3025fa88fce3f03c586
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-20 04:01:47 -0800

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef _MAIN_H_
+#define _MIAN_H_
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,4 +11,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 
-#endif
+#endif 
