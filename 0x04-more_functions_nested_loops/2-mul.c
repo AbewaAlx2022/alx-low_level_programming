c0f897d0e04db22c72a681e734cfea6b300d421f
AbewaAlx2022 <bonju0042@gmail.com>
2022-12-20 00:16:01 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function that multipliestwo numbers
+ * @a: first nub=mber
+ * @b: second number
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
