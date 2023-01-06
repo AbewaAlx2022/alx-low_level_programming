f18d4e7279b64f18b1f6d192c1fdfd6e26dc74d1
AbewaAlx2022 <bonju0042@gmail.com>
2023-01-06 06:00:33 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - The progrm that maultiplies two numbers
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
+	{
+		printf("Error\n")
+			return (1);
+	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atio(agrv[index]);
+	}
+	pritnf("%d\n", multiplication);
+	return (0);
+}
