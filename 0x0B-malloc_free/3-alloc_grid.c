e39130071bb99cfb2e73297591a9fa32e890c699
AbewaAlx2022 <bonju0042@gmail.com>
2023-01-09 01:15:40 -0800

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	mee = malloc(sizeof(int *) * height)
+	mee = malloc(sizeof(int *) * height);
 		if (mee == NULL)
 			return (NULL);
 	for (x = 0; x < height; x++)
@@ -33,5 +33,5 @@ int **alloc_grid(int width, int height)
 		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
 	}
-	return (mee)
+	return (mee);
 }
